0fd9d0ef45a8fc2f6e8958ef853d9b8e62753728
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2024-05-09 20:11:43 +0600

    ran the project

--- public/models.py
+++ public/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 # Create your models here.
-class Register(models.Model):
+class Account(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100, unique=True, null=True)
--- public/urls.py
+++ public/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from views import Home
+from .views import Home
 
 app_name = "public"
 
