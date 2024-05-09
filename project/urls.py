73fdef79b92ee55bd172aa2e7d44dd47450734d4
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2024-05-09 18:08:00 +0600

    refactored

--- project/urls.py
+++ project/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for public project.
+URL configuration for quantum_mind project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
--- project/wsgi.py
+++ project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for public project.
+WSGI config for quantum_mind project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "public.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
 application = get_wsgi_application()
