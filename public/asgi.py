036bc74a28624713b3c7aec2af257baa8078c388
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2024-05-09 16:57:57 +0600

    baseline changed from model name to public

--- public/asgi.py
+++ public/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for quantum_mind project.
+ASGI config for public project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quantum_mind.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'public.settings')
 
 application = get_asgi_application()
