aa08e59ba910c43605ba832ae8bd3ae96dce1a80
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2024-06-11 07:16:53 +0600

    ollama package added

--- services/data/repository/default_ollama_no_stream.py
+++ services/data/repository/default_ollama_no_stream.py
@@ -1,8 +1,7 @@
 import logging
 from typing import Optional
-
-import requests
-from requests.exceptions import ConnectionError, Timeout, RequestException
+import ollama
+from ollama import Client
 
 from services.domain.repository import ChatRepository
 
@@ -13,57 +12,57 @@ class DefaultOllamaNoStream(ChatRepository):
     def __init__(self, host: str, model: str = "gemma:2b"):
         self.host = host
         self.model = model
+        self.client = Client(host=host)
 
     def set_model(self, model: str) -> None:
         self.model = model
 
     def generate(self, input_text: str) -> str:
-        url = f"{self.host}/api/generate"
-        data = {"model": self.model, "prompt": input_text, "stream": False}
-        logger.debug(f"logged -> {url=}, {data=}")
-        try:
-            response = requests.post(url, json=data)
-        except (ConnectionError, Timeout, RequestException) as e:
-            raise RuntimeError(f"Could not generate model response: {e}")
-
-        return response.json()["response"]
+        # try:
+        logger.debug(
+            f"{self.client.generate(model=self.model, prompt=input_text, stream=False)=}"
+        )
+        response = self.client.generate(
+            model=self.model, prompt=input_text, stream=False
+        )
+        # except (ollama.RequestError, ollama.ResponseError) as e:
+        #     raise RuntimeError(f"Could not generate ai chat: {e}")
+        return response["response"]
 
     def complete(self, chat_history: list[str], context: str) -> str:
-        url = f"{self.host}/api/chat"
         messages = []
-        for idx, message in enumerate(chat_history):
-            role = "assistant" if (idx & 1) else "user"
+        for i, message in enumerate(chat_history):
+            role = "user" if i % 2 == 0 else "assistant"
             messages.append({"role": role, "content": message})
-        messages.append({"role": "user", "content": context})
-        data = {"model": self.model, "messages": messages, "stream": False}
-        try:
-            response = requests.post(url, json=data)
-        except (ConnectionError, Timeout, RequestException) as e:
-            raise RuntimeError(f"Could not complete ai chat: {e}")
 
-        return response.json()["response"]
+        messages.append({"role": "user", "content": context})
+        try:
+
+            response = self.client.chat(
+                model=self.model, messages=messages, stream=False
+            )
+        except (ollama.RequestError, ollama.ResponseError) as e:
+            raise RuntimeError(f"Could not complete ai chat: {e}")
+        return response["message"]["content"]
 
     def create_model(self, model_name: str, parameters: dict) -> str:
         pass
 
     def list_local_models(self) -> list[dict]:
-        url = f"{self.host}/api/tags"
         try:
-            response = requests.get(url)
-        except (RequestException, ConnectionError, Timeout) as e:
+            response = self.client.list()
+        except (ollama.RequestError, ollama.ResponseError) as e:
             raise RuntimeError(f"Could not list local models: {e}")
-        return response.json()["models"]
+        return response["models"]
 
     def show_model_information(self, model_name: Optional[str]) -> dict:
         if not model_name:
             model_name = self.model
-        url = f"{self.host}/api/show"
-        data = {"name": model_name}
         try:
-            response = requests.post(url, json=data)
-        except (ConnectionError, Timeout, RequestException) as e:
+            response = self.client.show(model=model_name)
+        except (ollama.RequestError, ollama.ResponseError) as e:
             raise RuntimeError(f"Could not show model information: {e}")
-        return response.json()
+        return response
 
     def copy_model(self, source_model: str, destination_model: str) -> str:
         pass
