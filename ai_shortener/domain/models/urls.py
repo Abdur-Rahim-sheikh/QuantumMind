4c40d3a0b57e67abda799f74063e20c9581fe210
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-03-21 06:03:28 +0600

    feat: url models setup

---
+++ ai_shortener/domain/models/urls.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+from django.contrib.auth.models import User
+import base64
+class Urls(BaseModel):
+    user: User
+    title: str
+    description: str = None
+    image: base64 = None
+    long_url: str
+    short_url: str
+    ai_url: str = None
