a72fc682a2289635d8385d1fa9a2dd73acdb605a
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-03-22 21:23:34 +0600

    feat: delete and put api

--- public/frontend_api/ai_shortener_api.py
+++ public/frontend_api/ai_shortener_api.py
@@ -1,8 +1,10 @@
+import json
+
 from django.views import View
 
 from ai_shortener.di.use_case import UseCase
 from public.utils import json_response
-import json
+
 
 class AIShortener(View):
     def __init__(self):
@@ -40,4 +42,41 @@ class AIShortener(View):
             message="Successfully created the url",
             data={"url": url},
             status_code=201,
+        )
+
+    def delete(self, request):
+        try:
+
+            data = json.loads(request.body)
+            url_id = data.get("url_id")
+        except RuntimeError as e:
+            return json_response(
+                success=False, message="Invalid message", status_code=400
+            )
+
+        self.__use_case.delete_url(pid=url_id)
+        return json_response(
+            success=True, message="Successfully deleted the url", status_code=204
+        )
+
+    def put(self, request):
+        try:
+            data = json.loads(request.body)
+            url_id = data.get("url_id")
+            title = data.get("title", None)
+            description = data.get("description", None)
+            img = data.get("img", None)
+        except RuntimeError as e:
+            return json_response(
+                success=False, message="Invalid message", status_code=400
+            )
+
+        self.__use_case.update_url(
+            pid=url_id,
+            title=title,
+            description=description,
+            img=img,
+        )
+        return json_response(
+            success=True, message="Successfully updated the url", status_code=204
         )
